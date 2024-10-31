54b221f5a320c1955840b8c9d11c3da1d54da8f7
Moonrise <aliuesch@gmail.com>
2024-10-31 10:58:17 -0700

    updated documentation and added arduino version of the code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# UA Solar Tracker 2024
+# UA Solar Tracker Project 2024
 ## Group 7 - Sun Sizzlers
+
+## Folder Layout:
+
+### CAD:
+This folder has all of the 3d model files, the model which is currently in use is ``CURRENT CASE.3mf``. You can safely ignore the others for now.
+
+### Software:
+
+#### Arduino:
+This folder has the ``.ino`` versions of the main code which can be directly imported into the *Arduino IDE*.
+
+#### In-Class Code, Main Code, and PhotoresistorTesting:
+These are all project folders for the software I'm using to write code, said software is called *PlatformIO* and it's an extension for VSCode. It lets me write code on different computers without having to worry about tweaking settings every time.
---
+++ Software/Arduino/SolarTracker/SolarTracker.ino
@@ -0,0 +1,114 @@
+/*
+Solar Tracker Code Prototype
+
+Written by Adam Esch
+
+Last edited: 10/29/2024
+
+ARDUINO IDE VERSION
+
+TODO:
+- Add Code for secondary axes
+
+*/
+
+//Library Setup
+#include <Servo.h>
+
+//Variables
+const int stepSize = 10; //In degrees
+
+//Pins:
+const int xyServo = 9;
+//const int zServo = 10;
+
+const int pr1 = A0;
+const int pr2 = A1;
+
+//Servo Setup
+const int servoMin = 0;
+const int servoMax = 180;
+
+Servo xy;
+//Servo z;
+
+
+// Read our photoresistor, specified by the pin argument, a set number of times and then average the value so we get better data
+float getData(int pin,int samples){
+  float result = 0.0;
+
+  for (int i = 0; i < samples; i++){
+    result += analogRead(pin);
+  }
+
+  return result / samples;
+}
+
+// Primary Function
+void findSun(int delayTime = 100,int samples = 100){
+
+  float pr1Max = 0.0;
+  float pr2Max = 0.0;
+
+  float sensorSum = 0.0;
+
+  float maxIntensity = 0.0;
+
+  int xyThetaRef = 0;
+
+  // Primary Loop, we set the servo to 0 degrees, defined by servoMin, and step the servo by a set number of degrees defined by stepSize, till we hit 180 degrees (servoMax)
+  for(int xyTheta = servoMin; xyTheta < servoMax; xyTheta += stepSize){
+
+    // For each degree step that we do, read the photoresistors with the previous function that gives us better data
+    pr1Max = getData(pr1, samples);
+    pr2Max = getData(pr2, samples);
+
+    // Sum the sensor data
+    sensorSum = pr1Max + pr2Max;
+
+    // To save on memory, we check to see if the combined sensor data is higher than previous readings (thus indicating being in brighter light)
+    // If we find a new highest value, the program stores the angle so we can return back to it later
+    if(sensorSum > maxIntensity){
+      maxIntensity = sensorSum;
+      xyThetaRef = xyTheta;
+    }
+
+    //Step the servo and then wait a little (the waiting is for effect, realistically it doesn't really need such a long delay)
+    xy.write(xyTheta);
+    delay(delayTime);
+  }
+
+  // After the loop, the program now has an angle stored where the arduino sensed the highest intensity light, so let's go to it
+  xy.write(xyThetaRef);
+
+}
+
+void setup() {
+
+  //Pin Setup
+  pinMode(xyServo, OUTPUT);
+  //pinMode(zServo, OUTPUT);
+
+  pinMode(pr1, INPUT);
+  pinMode(pr2, INPUT);
+
+  //Servo Startup
+  xy.attach(xyServo);
+  //z.attach(zServo);
+
+  //Homing
+  xy.write(servoMin);
+
+  //Wait for a little so people can get out of the way so the tracker can run unobstructed
+  delay(5000);
+
+  //Run our rough finding function
+  findSun(100,100);
+
+  //fineTuning();
+  
+}
+
+//Now do nothing, for now
+void loop() {
+}
